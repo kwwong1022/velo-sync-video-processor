7096641ff99d88fceff0e5c001ae02944f31bb99
Kenneth Wong <kwwong2210@gmail.com>
2024-03-04 19:01:54 +0800

    Add boto3 lib

--- handler.py
+++ handler.py
@@ -1,12 +1,23 @@
 import json
+import boto3
 
 def app(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+    # boto3 test code
+    session = boto3.Session()
 
+    s3 = session.resource('s3')
+
+    my_bucket = s3.Bucket('velo-sync-storage-bucket-dev')
+
+    for my_bucket_object in my_bucket.objects.all():
+        print(my_bucket_object.key)
+
+
+    # Health check result
     return {
         "statusCode": 200, 
-        "body": json.dumps(body)
+        "body": json.dumps({
+            "message": "Go Serverless v3.0! Your function executed successfully!",
+            "input": event,
+        })
     }
